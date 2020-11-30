cb2f990b21bf948335e69a43bfe9d7a9cbdf282a
nguyenbim <51375333+nguyenbim@users.noreply.github.com>
2020-11-30 15:49:42 +0700

    Update Invoice.java

--- Test Plan/AIMS-master/aims/AIMS-Student/src/entity/invoice/Invoice.java
+++ Test Plan/AIMS-master/aims/AIMS-Student/src/entity/invoice/Invoice.java
@@ -1,12 +1,13 @@
 package entity.invoice;
 
 import entity.order.Order;
+import entity.order.RushOrder;
 
 public class Invoice {
 
     private Order order;
     private int amount;
-    
+    private RushOrder rushOrder;
     public Invoice(){
 
     }
@@ -14,11 +15,15 @@ public class Invoice {
     public Invoice(Order order){
         this.order = order;
     }
-
+    public Invoice(RushOrder rushOrder){
+        this.rushOrder = rushOrder;
+    }
     public Order getOrder() {
         return order;
     }
-
+    public  RushOrder getRushOrder(){
+        return rushOrder;
+    }
     public void setAmount(int amount) {
         this.amount = amount;
     }
