408e2c9cfded6aee6ad83c9a20400e70054e8594
nguyenbim <51375333+nguyenbim@users.noreply.github.com>
2020-12-01 10:50:38 +0700

    Update PlaceRushOrderController.java

--- Test Plan/AIMS-master/aims/AIMS-Student/src/controller/PlaceRushOrderController.java
+++ Test Plan/AIMS-master/aims/AIMS-Student/src/controller/PlaceRushOrderController.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.text.ParseException;
-import java.text.DateFormat;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
@@ -25,6 +24,7 @@ import common.exception.InvalidDeliveryInfoException;
 import entity.invoice.Invoice;
 import entity.order.Order;
 import entity.order.OrderMedia;
+import entity.order.RushOrder;
 import views.screen.popup.PopupScreen;
 
 /**
@@ -44,16 +44,47 @@ import views.screen.popup.PopupScreen;
 
 public class PlaceRushOrderController extends BaseController{
 
-    /**
-     * The method validates the info
-     * @param info Information for rush order
-     * @throws InterruptedException
-     * @throws IOException
-     */
-    public void validateRushDeliveryInfo(HashMap<String, String> info) throws InterruptedException, IOException {
 
+
+    /**
+     * Just for logging purpose
+     */
+    private static Logger LOGGER = utils.Utils.getLogger(PlaceRushOrderController.class.getName());
+
+    /**
+     * This method checks the avalibility of product when user click PlaceOrder button
+     * @throws SQLException
+     */
+    public void placeRushOrder() throws SQLException{
+        Cart.getCart().checkAvailabilityOfProduct();
     }
 
+
+    /**
+     * This method creates the new rushOrder based on the Cart
+     * @return rushOrder
+     * @throws SQLException
+     */
+    public RushOrder createRushOrder() throws SQLException{
+        RushOrder rushOrder = new RushOrder();
+        for (Object object : Cart.getCart().getListMedia()) {
+            CartMedia cartMedia = (CartMedia) object;
+            OrderMedia orderMedia = new OrderMedia(cartMedia.getMedia(),
+                    cartMedia.getQuantity(),
+                    cartMedia.getPrice());
+            rushOrder.getlstOrderMedia().add(orderMedia);
+        }
+        return rushOrder;
+    }
+
+    /**
+     * This method creates the new Invoice based on rush order
+     * @param rushOrder
+     * @return Invoice
+     */
+    public Invoice createInvoice(RushOrder rushOrder) {
+        return new Invoice(rushOrder);
+    }
     /**
      * This method validates received datetime for rush order
      * @param fromTime lower bounder
@@ -61,6 +92,28 @@ public class PlaceRushOrderController extends BaseController{
      * @param date datetime
      * @return true if valid
      */
+    /**
+     * This method takes responsibility for processing the shipping info from user
+     * @param info
+     * @throws InterruptedException
+     * @throws IOException
+     */
+    public void processDeliveryInfo(HashMap info) throws InterruptedException, IOException{
+        LOGGER.info("Process Delivery Info");
+        LOGGER.info(info.toString());
+        validateDeliveryInfo(info);
+    }
+
+    /**
+     * The method validates the info
+     * @param info
+     * @throws InterruptedException
+     * @throws IOException
+     */
+    public void validateDeliveryInfo(HashMap<String, String> info) throws InterruptedException, IOException{
+
+    }
+
     public boolean validateReceiveDateTime (String fromTime, String toTime, String date) {
         String fromDateTime  = fromTime + " " + date;
         String toDateTime = toTime + " " + date;
